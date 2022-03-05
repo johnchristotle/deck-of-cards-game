0391696e8ee609fd150eef390036a26d90a14a0b
agholor-christotle <agholorchristotle@gmail.com>
2022-03-05 13:36:57 -0800

    functionality code added

---
+++ main.js
@@ -0,0 +1,67 @@
+var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var suits = ["diamonds", "hearts", "spades", "clubs"];
+var deck = new Array();
+
+function getDeck()
+{
+	var deck = new Array();
+
+	for(var i = 0; i < suits.length; i++)
+	{
+		for(var x = 0; x < cards.length; x++)
+		{
+			var card = {Value: cards[x], Suit: suits[i]};
+			deck.push(card);
+		}
+	}
+
+	return deck;
+}
+
+function shuffle()
+{
+	// for 1000 turns
+	// switch the values of two random cards
+	for (var i = 0; i < 1000; i++)
+	{
+		var location1 = Math.floor((Math.random() * deck.length));
+		var location2 = Math.floor((Math.random() * deck.length));
+		var tmp = deck[location1];
+
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
+	}
+
+	renderDeck();
+}
+
+function renderDeck()
+{
+	document.getElementById('deck').innerHTML = '';
+	for(var i = 0; i < deck.length; i++)
+	{
+		var card = document.createElement("div");
+		var value = document.createElement("div");
+		var suit = document.createElement("div");
+		card.className = "card";
+		value.className = "value";
+		suit.className = "suit " + deck[i].Suit;
+
+		value.innerHTML = deck[i].Value;
+		card.appendChild(value);
+		card.appendChild(suit);
+
+		document.getElementById("deck").appendChild(card);
+	}
+}
+
+function load()
+{
+	deck = getDeck();
+	shuffle();
+	renderDeck();
+}
+
+window.onload = load;
+                
+            
